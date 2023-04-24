4d3e77bf980ce4106926e902f604deac8d20d93a
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-04-24 17:06:18 +0200

    1. commit

---
+++ src/main/java/com/porkoltblog/PorkoltblogApplication.java
@@ -0,0 +1,13 @@
+package com.porkoltblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorkoltblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorkoltblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porkoltblog/PorkoltblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porkoltblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorkoltblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
