caa5bf948da397aaceda859b3ae1a430a78b4c93
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-04-26 10:42:02 +0200

    1.9 ver

--- src/main/java/com/porkoltblog/PorkoltController/PorkoltController.java
+++ src/main/java/com/porkoltblog/PorkoltController/PorkoltController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.porkoltblog.entity.Porkolt;
@@ -31,7 +33,8 @@ public class PorkoltController {
 		
 	}
 	
-	public Porkolt createPorkolt(Porkolt porkolt) {
+	@PostMapping("/mentes")
+	public Porkolt createPorkolt(@RequestBody Porkolt porkolt) {
 		return service.createPorkolt(porkolt);
 		
 	}
