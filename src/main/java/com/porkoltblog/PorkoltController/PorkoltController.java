f967b76f8dd55583cd62ed9ae06e830f271f2803
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-05-08 13:11:09 +0200

    v2.1

--- src/main/java/com/porkoltblog/PorkoltController/PorkoltController.java
+++ src/main/java/com/porkoltblog/PorkoltController/PorkoltController.java
@@ -3,7 +3,6 @@ package com.porkoltblog.PorkoltController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,18 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.porkoltblog.entity.Porkolt;
 import com.porkoltblog.service.PorkoltService;
 
 
 
-@Controller
+@RestController
+@RequestMapping("/porkoltblog")
 public class PorkoltController {
 	
 	@Autowired
 	PorkoltService service;
-	
+	/*
 	@RequestMapping("/porkoltblog")
 	public String PorkoltBolog(Model model) {
 		
@@ -30,18 +32,19 @@ public class PorkoltController {
 		return "porkoltblog";
 		
 	}
-	
+	*/
+	@GetMapping("/porkoltlista")
 	public List<Porkolt> getReceptek(){
 		return service.getPorkoltek();
 		
 	}
 	
 	@PostMapping("/mentes")
-	public Porkolt createPorkolt(@RequestBody Porkolt porkolt) {
+	public Porkolt createPorkolt( @RequestBody Porkolt porkolt) {
 		return service.createPorkolt(porkolt);
 		
 	}
-	@GetMapping("/kereses/{id}")
+	@GetMapping("/getporkolt/{id}")
 	public Porkolt getPorkolt(@PathVariable Long id) {
 		return service.getPorkolt(id);
 		
